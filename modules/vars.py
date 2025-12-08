cdb4bf1f70904bf3746cae77589558ef7781ca48
Yash-7777-uk <up.yash111@gmail.com>
2025-12-08 17:29:35 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,18 +3,18 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "12345"))
-API_HASH = environ.get("API_HASH", "x1x2x3")
+API_ID = int(environ.get("API_ID", "23801870"))
+API_HASH = environ.get("API_HASH", "9645cfafdfc9be9a7a46fb4874992cf6")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "12345"))
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
+OWNER = int(environ.get("OWNER", "6877021488"))
+CREDIT = environ.get("CREDIT", "𝕊𝔻𝕍 𝘽𝙊𝙏𝙎")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '12345').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '6877021488').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '12345').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '6877021488').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
